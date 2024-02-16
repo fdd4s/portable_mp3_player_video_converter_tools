dd4621e4a19eddace73ec8196a06193a9dc78104
fdd4s <114248002+fdd4s@users.noreply.github.com>
2024-02-16 15:32:33 +0100

    Update video2actions18.sh

--- video2actions18.sh
+++ video2actions18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 name="${1%.*}"
 
-ffmpeg -i "$1" -vf "scale=-2:128, crop=160:128" -r 14 -pix_fmt yuv420p -c:v amv -c:a adpcm_ima_amv -ac 1 -ar 22050 -block_size 1575 "$name"-p.amv
+ffmpeg -i "$1" -vf "scale=-2:128, crop=160:128" -r 14 -pix_fmt yuvj420p -c:v amv -c:a adpcm_ima_amv -ac 1 -ar 22050 -block_size 1575 "$name"-p.amv
 
 echo "$name"-p.amv created
 
